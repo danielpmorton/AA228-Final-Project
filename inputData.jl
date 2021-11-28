4b6db03b7612e550ce39bb65efc64ed9115a46c4
Walter <wjmanuel@gmail.com>
2021-11-27 22:13:02 -0800

    Made new function "createCleanWeeklyData" that replaces every bye/injury week with the previous week's score

--- inputData.jl
+++ inputData.jl
@@ -61,14 +61,59 @@ function createWeeklyData(year)
     return newData
 end
 
+function createCleanWeeklyData(year)
+
+    # select statistic to track
+    # 19 = HalfPPR, 18 = Standard, 17 = PPR
+    pointType = 19
+
+    pathstring = string("weekly//", year)
+    weeklist = readdir(pathstring)
+    numWeeks = 17
+
+    week = ["week1","week2","week3","week4","week5","week6","week7",
+            "week8","week9","week10","week11","week12","week13",
+            "week14","week15","week16","week17"]
+    newData = DataFrame();
+    players = []
+    positions = []
+
+    for i = 1:numWeeks
+        fullpath = string(pathstring,"//",weeklist[i])
+        rawWeek = rawInput(fullpath)
+        points = []
+        if i == 1
+            players = getPlayerNames(rawWeek)
+            positions = getPositions(rawWeek)
+            newData.player = players
+            newData.position = positions
+        end
+        for j = 1:length(players)
+            push!(points,0)
+            for k = 1:size(rawWeek,1)
+                if players[j] == rawWeek.Player[k]
+                    points[j] = rawWeek[k,pointType]
+                end
+            end
+            if points[j] == 0 && i > 1
+                points[j] = newData[j,week[max(i-1,1)]]
+            end
+        end
+        newData[!,week[i]] = points
+    end
+
+    return newData
+end
+
 function selectGroup(subnames,weeklydata)
-    subGroup = weeklydata
+    subGroup = copy(weeklydata)
+
     deleteIndex = []
 
     for i = 1:size(subGroup,1)
         exclude = true
         for j = 1:length(subnames)
-            if weeklydata.player[i] == subnames[j]
+            if subGroup.player[i] == subnames[j]
                 exclude = false
             end
         end
@@ -101,3 +146,6 @@ subsetNames = vcat(QBs, RBs, WRs)
 
 subset2019 = selectGroup(subsetNames,weekly2019)
 subset2018 = selectGroup(subsetNames,weekly2018)
+
+
+weekly2019clean = createCleanWeeklyData(years[end])
